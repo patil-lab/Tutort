3bd8526b9c9f8c64470014e8a6ff2e0a067b9372
patilbasavashree <patil@funpodium.net>
2021-11-11 16:40:03 +0800

    commit

--- src/assignment1/Encoding.java
+++ src/assignment1/Encoding.java
@@ -18,11 +18,7 @@ public class Encoding {
 			}
 
 			final String combine = userName.concat(":").concat(password);
-			String encode="RlVOODg6MjFUaHVyczU2";
 			System.out.println(Base64.getEncoder().encodeToString(combine.getBytes(StandardCharsets.UTF_8)));
-			byte[] decodedBytes = Base64.getDecoder().decode(encode);
-			String decodedString = new String(decodedBytes);
-			System.out.println(decodedString);
 		}
 	}
 }
